91708aaaf234138ecf831058d419fd353d91b69e
Anna Royer <anna.royer@colorado.edu>
2018-02-07 15:34:09 -0700

    add validation to course and add relationships test to course model spec

---
+++ spec/models/course_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe Course, type: :model do
+  context 'validations' do
+    it { should validate_presence_of :name }
+  end
+
+  context 'relationships' do
+    it { should have_many :student_courses }
+    it { should have_many :students }
+  end
+end
