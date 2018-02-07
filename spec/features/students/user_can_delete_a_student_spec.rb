2b0a529ba8dc8aa4d357f400ddf9ea9f2715137c
Anna Royer <anna.royer@colorado.edu>
2018-02-07 14:26:24 -0700

    reconfigure test after adding navbar

--- spec/features/students/user_can_delete_a_student_spec.rb
+++ spec/features/students/user_can_delete_a_student_spec.rb
@@ -3,16 +3,13 @@ require 'rails_helper'
 describe 'As a user' do
   context 'when I visit students index and click delete next to a student' do
     scenario 'I can delete that student' do
-      students = create_list(:student, 2)
+      student = create(:student)
 
       visit students_path
-      students.each do |student|
-        expect(page).to have_content(student.name)
-      end
-      within(first('ul')) { click_on 'Delete' }
+      expect(page).to have_content(student.name)
+      click_on 'Delete'
 
-      expect(page).to_not have_content(students.first.name)
-      expect(page).to have_content(students.last.name)
+      expect(page).to_not have_content(student.name)
     end
   end
 end
