a574e77bdc8dab4e97bdd55ae6df915571854de4
Anna Royer <anna.royer@colorado.edu>
2018-02-07 13:44:58 -0700

    add create method and private student params method

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,8 +12,22 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "#{@student.name} created!"
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+  end
+
   private
   def set_student
     @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:name)
   end
 end
