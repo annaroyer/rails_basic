88f353e879ac1fb9acfc27f01b85acb3fedd28c5
Anna Royer <anna.royer@colorado.edu>
2018-02-07 14:31:33 -0700

    add new action to addresses controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,3 +1,12 @@
 class AddressesController < ApplicationController
+  before_action :set_student, only: :new
 
+  def new
+    @address = @student.addresses.new
+  end
+
+  private
+  def set_student
+    @student = Student.find(params[:student_id])
+  end
 end
