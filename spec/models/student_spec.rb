2c1522531eb47eb0d90700214a2e1565ad5ed807
Anna Royer <anna.royer@colorado.edu>
2018-02-07 15:27:31 -0700

    add relationship test to student model

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -4,4 +4,10 @@ describe Student, type: :model do
   context 'validations' do
     it { should validate_presence_of :name }
   end
+
+  context 'relationships' do
+    it { should have_many :addresses }
+    it { should have_many :student_courses }
+    it { should have_many :courses }
+  end
 end
