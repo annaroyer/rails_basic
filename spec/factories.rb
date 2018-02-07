e8b7e88916f1f5c52b1d5efe54656493bb153684
Anna Royer <anna.royer@colorado.edu>
2018-02-07 14:53:44 -0700

    add test for user sees all a students addresses

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,14 @@
 FactoryBot.define do
-
   factory :student do
     sequence(:name) { |n| "Name #{n}" }
   end
+
+  factory :address do
+    sequence(:description) { |n| "description #{n}" }
+    sequence(:street) { |n| "street #{n}" }
+    sequence(:city) { |n| "city #{n}" }
+    sequence(:zip_code) { |n| n }
+    student
+  end
+
 end
--- spec/features/students/user_sees_a_student_spec.rb
+++ spec/features/students/user_sees_a_student_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 describe 'As a user' do
   context 'when I visit students index and click a students name' do
-    scenario 'I can see the name of that student' do
+    scenario 'I can see the name of that student and all their addresses' do
       student = Student.create!(name: 'Billy Jean')
+      addresses = create_list(:address, 2, student: student)
 
       visit students_path
       click_on student.name
 
       expect(page).to have_content(student.name)
+      addresses.each do |address|
+        expect(page).to have_content(address.description)
+      end
     end
   end
 end
