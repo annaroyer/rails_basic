d78b9597bf3b0aacbdabb52cd069235b8f267a77
Anna Royer <anna.royer@colorado.edu>
2018-02-07 13:23:04 -0700

    add show and set student methods to model

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,12 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :delete, :update]
 
+  def show
+
+  end
+
+  private
+  def set_student
+    @student = Student.find(params[:id])
+  end
 end
