49ef606d3ca438fd0d2c1b9315dea9dd72a09566
Anna Royer <anna.royer@colorado.edu>
2018-02-07 13:40:25 -0700

    add new action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,11 +2,14 @@ class StudentsController < ApplicationController
   before_action :set_student, only: [:show, :edit, :delete, :update]
 
   def show
-
   end
 
   def index
     @students = Student.all
+  end
+
+  def new
+    @student = Student.new
   end
 
   private
