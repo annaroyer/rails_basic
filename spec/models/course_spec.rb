73c173e10d5f3d7f07dc74fa040b1e8b78e6c0a2
Anna Royer <anna.royer@colorado.edu>
2018-02-07 15:34:53 -0700

    add uniqueness validation spec to course

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -5,6 +5,10 @@ describe Course, type: :model do
     it { should validate_presence_of :name }
   end
 
+  context 'uniqueness' do
+    it { should validate_uniqueness_of :name }
+  end
+
   context 'relationships' do
     it { should have_many :student_courses }
     it { should have_many :students }
