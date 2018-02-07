b03f119eaaa28dfb4b91f77d9f0a852e237327cd
Anna Royer <anna.royer@colorado.edu>
2018-02-07 14:12:23 -0700

    add test for student name links

--- spec/features/students/user_sees_a_student_spec.rb
+++ spec/features/students/user_sees_a_student_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
 describe 'As a user' do
-  context 'when I visit student show' do
+  context 'when I visit students index and click a students name' do
     scenario 'I can see the name of that student' do
       student = Student.create!(name: 'Billy Jean')
 
-      visit student_path(student)
+      visit students_path
+      click_on student.name
 
       expect(page).to have_content(student.name)
     end
   end
 end
+
+
+# Navigation
+# As a user
+# When I visit any page
+# I see links to see a list of all students, or create a new student
+#
+# As a user
+# When I visit /students
+# And I click on a student’s name
+# I am taken to a show page for that student
