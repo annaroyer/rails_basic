bd1bce9e889887ef103206d1d0a5454c9bd9bb23
Anna Royer <anna.royer@colorado.edu>
2018-02-07 14:28:20 -0700

    add new to addresses routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students do
-    resources :addresses, only: [:create]
+    resources :addresses, only: [:new, :create]
   end
 end
