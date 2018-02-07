70deec238b73164616e2b5c2de1fd533930e215a
Anna Royer <anna.royer@colorado.edu>
2018-02-07 15:08:54 -0700

    add courses table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180207213520) do
+ActiveRecord::Schema.define(version: 20180207220825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 20180207213520) do
     t.integer "zip_code"
     t.bigint "student_id"
     t.index ["student_id"], name: "index_addresses_on_student_id"
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "students", force: :cascade do |t|
